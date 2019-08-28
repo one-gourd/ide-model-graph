1028cc1a61acffa5ef8c2add4dfc12a20dbe5c52
bocheng.zbc <bocheng.zbc@alibaba-inc.com>
2019-08-28 16:40:58 +0800

    feat: 🎸 功能新增: vertex 模型新增 clone 方法

--- src/ModelGraph/vertex.ts
+++ src/ModelGraph/vertex.ts
@@ -4,20 +4,38 @@ import {
   Instance,
   IAnyModelType,
   SnapshotOrInstance,
-  detach
+  detach,
+  clone
 } from 'mobx-state-tree';
 
-import { quickInitModel, JSONModel, EMPTY_JSON_SNAPSHOT } from 'ide-model-utils';
-
+import {
+  quickInitModel,
+  JSONModel,
+  EMPTY_JSON_SNAPSHOT
+} from 'ide-model-utils';
 
 import { PREFIX } from './constant';
 import { EdgeModel, IEdgeModel, IEdgeModelSnapshot } from './edge';
+import { invariant } from 'ide-lib-utils';
+
+// 默认是返回当前 id 数值
+export const DEFAULT_ID_CLONER = function(id: string) {
+  return id;
+};
+export const DEFAULT_META_CLONER = function(meta: any) {
+  return meta;
+};
+
+export interface ICloneFns {
+  cloneId?: typeof DEFAULT_ID_CLONER;
+  cloneMeta?: typeof DEFAULT_META_CLONER;
+}
 
 /**
  * 节点 model
  */
 export const VertexModel: IAnyModelType = quickInitModel('VertexModel', {
-  // 规定每个节点 id 
+  // 规定每个节点 id
   id: types.identifier,
 
   // 节点 meta 信息：比如： {name, icon}
@@ -25,147 +43,199 @@ export const VertexModel: IAnyModelType = quickInitModel('VertexModel', {
 
   // 当前节点的边
   edges: types.array(types.late((): IAnyModelType => EdgeModel))
-}).views(self => {
-  return {
+})
+  .views(self => {
+    return {
+      // 必须要以 `V_` 作为前缀
+      get key() {
+        return `${PREFIX.VERTEX}${self.id}`;
+      },
+      /**
+       * 获取 degree 数值
+       *
+       */
+      get degree() {
+        return this.edges.length;
+      },
 
-   // 必须要以 `V_` 作为前缀
-  get key() {
-      return `${PREFIX.VERTEX}${self.id}`
-  },
-  /**
-   * 获取 degree 数值
-   *
-   */
-    get degree(){
-      return this.edges.length;
-    },
+      /**
+       * 获取所有邻接节点 id
+       * 因为是通过 edge 来定位的，然而在 mst 中只能通过 id
+       */
+      get neighborIds() {
+        const edges = self.edges;
+        return edges.map((edge: IEdgeModel) => {
+          return edge.startVid === self.id ? edge.endVid : edge.startVid;
+        });
+      }
+    };
+  })
+  .views(self => {
+    return {
+      /**
+       * 根据边 id 找到指定的边模型
+       *
+       * @param {string} id - id
+       * @returns
+       */
+      findEdgeById(id: string) {
+        return self.edges.find((t: IEdgeModel) => t.id === id) || null;
+      },
 
-  /**
-   * 获取所有邻接节点 id
-   * 因为是通过 edge 来定位的，然而在 mst 中只能通过 id
-   */
-    get neighborIds() {
-      const edges = self.edges;
-      return edges.map((edge: IEdgeModel) => {
-        return edge.startVid === self.id
-          ? edge.endVid
-          : edge.startVid;
-      });
-    }
-  };
-}).views(self => {
-  return {
+      /**
+       * 根据指定 节点 找到指定的边模型
+       * 需要开始节点或结束节点匹配即可
+       *
+       * @param {IVertexModelSnapshot} vertex - 节点类型
+       * @returns
+       */
+      findEdgeByVertex(vertex: IVertexModelSnapshot) {
+        return (
+          self.edges.find(
+            (t: IEdgeModel) =>
+              t.startVid === vertex.id || t.endVid === vertex.id
+          ) || null
+        );
+      },
 
-  /**
-   * 根据边 id 找到指定的边模型
-   *
-   * @param {string} id - id
-   * @returns
-   */
-    findEdgeById(id: string) {
-      return self.edges.find((t: IEdgeModel) => t.id === id) || null;
-    },
+      /**
+       * 根据指定 节点 id 找到指定的边模型
+       * 需要开始节点或结束节点匹配即可
+       *
+       * @param {IVertexModelSnapshot} vertex - 节点类型
+       * @returns
+       */
+      findEdgeByVid(vid: string) {
+        return (
+          self.edges.find(
+            (t: IEdgeModel) => t.startVid === vid || t.endVid === vid
+          ) || null
+        );
+      },
 
-  /**
-   * 根据指定 节点 找到指定的边模型
-   * 需要开始节点或结束节点匹配即可
-   *
-   * @param {IVertexModelSnapshot} vertex - 节点类型
-   * @returns
-   */
-    findEdgeByVertex(vertex: IVertexModelSnapshot) {
-      return self.edges.find((t: IEdgeModel) => t.startVid === vertex.id || t.endVid === vertex.id) || null;
-    },
+      /**
+       * 根据边 找到指定的边模型；
+       * 需要开始节点和结束节点完全匹配
+       *
+       * @param {IEdgeModelSnapshot} edge
+       * @returns
+       */
+      findEdge(edge: IEdgeModelSnapshot) {
+        return (
+          self.edges.find(
+            (t: IEdgeModel) =>
+              t.startVid === edge.startVid && t.endVid === edge.endVid
+          ) || null
+        );
+      }
+    };
+  })
+  .views(self => {
+    return {
+      /**
+       * 判断某条边是否存在于当前节点上（通过 id）
+       *
+       * @param {IEdgeModelSnapshot} edge
+       * @returns {boolean}
+       */
+      hasEdge(edge: IEdgeModelSnapshot): boolean {
+        const edgeNode = self.findEdgeById(edge.id);
+        return !!edgeNode;
+      },
 
-  /**
-   * 根据指定 节点 id 找到指定的边模型
-   * 需要开始节点或结束节点匹配即可
-   *
-   * @param {IVertexModelSnapshot} vertex - 节点类型
-   * @returns
-   */
-    findEdgeByVid(vid: string) {
-      return self.edges.find((t: IEdgeModel) => t.startVid === vid || t.endVid === vid) || null;
-    },
+      /**
+       * 判断某个节点是否是和当前节点连接（通过比对节点 id 实现）
+       *
+       * @param {IVertexModelSnapshot} vertex
+       * @returns {boolean}
+       */
+      hasNeighbor(vertex: IVertexModelSnapshot): boolean {
+        const vertexNode = this.edges.find(
+          (edge: IEdgeModel) =>
+            edge.startVid === vertex.id || edge.endVid === vertex.id
+        );
+        return !!vertexNode;
+      }
+    };
+  })
+  .actions(self => {
+    return {
+      /**
+       * 克隆当前节点，需要保证 id 不一样
+       * 通过传入指定函数可以操作该节点的 meta 信息（诸如修改节点名字等副作用操作）
+       * @returns
+       */
+      clone(cloneFns: ICloneFns = {}) {
+        const {
+          cloneId = DEFAULT_ID_CLONER,
+          cloneMeta = DEFAULT_META_CLONER
+        } = cloneFns;
 
+        invariant(!!cloneId, '[vertex] 缺少 id clone 方法');
+        invariant(!!cloneMeta, '[vertex] 缺少 meta clone 方法');
 
-    /**
-     * 根据边 找到指定的边模型；
-     * 需要开始节点和结束节点完全匹配
-     *
-     * @param {IEdgeModelSnapshot} edge
-     * @returns
-     */
-    findEdge(edge: IEdgeModelSnapshot) {
-      return self.edges.find((t: IEdgeModel) => t.startVid === edge.startVid && t.endVid === edge.endVid) || null;
-    }
-  };
-}).views(self=>{
-  return {
+        // 先创建新的 vertex，使用 cloneId 方法生成新 id
+        const clonedVertex = VertexModel.create({
+          id: cloneId(self.id)
+        });
 
-    /**
-     * 判断某条边是否存在于当前节点上（通过 id）
-     * 
-     * @param {IEdgeModelSnapshot} edge
-     * @returns {boolean}
-     */
-    hasEdge(edge: IEdgeModelSnapshot): boolean {
-      const edgeNode = self.findEdgeById(edge.id);
-      return !!edgeNode;
-    },
+        // 同时调用 cloneMeta 生成新 meta，注意为了防止修改原 meta，这里需要 assign
+        clonedVertex.setMeta(cloneMeta(Object.assign({}, self.meta)));
 
+        // 同时需要挨个遍历 edges 集合，修改其中节点原 id 为现 id
+        const newEdges = self.edges.map((edge: IEdgeModel) => {
+          const clonedEdge = clone(edge);
 
+          if (clonedEdge.startVid == self.id) {
+            clonedEdge.setStartVid(clonedVertex.id);
+          }
+          if (clonedEdge.endVid == self.id) {
+            clonedEdge.setEndVid(clonedVertex.id);
+          }
 
-    /**
-     * 判断某个节点是否是和当前节点连接（通过比对节点 id 实现）
-     *
-     * @param {IVertexModelSnapshot} vertex
-     * @returns {boolean}
-     */
-    hasNeighbor(vertex: IVertexModelSnapshot): boolean {
-        const vertexNode = this.edges.find((edge: IEdgeModel) => edge.startVid === vertex.id || edge.endVid === vertex.id);
-      return !!vertexNode;
-    }
+          return clonedEdge;
+        });
 
-  }
-}).actions(self=>{
-  return {
+        // 将 edge 挂载到 cloneVertex 上
+        clonedVertex.setEdges(newEdges);
 
-    /**
-     * 将 edge 添加到当前节点的边链表中
-     *
-     * @param {IEdgeModelSnapshot} edge - edge 实例
-     * @returns
-     */
-    addEdge(edge: IEdgeModelSnapshot) {
-      self.edges.push(edge);
-      return self;
-    },
+        return clonedVertex;
+      },
 
+      /**
+       * 将 edge 添加到当前节点的边链表中
+       *
+       * @param {IEdgeModelSnapshot} edge - edge 实例
+       * @returns
+       */
+      addEdge(edge: IEdgeModelSnapshot) {
+        self.edges.push(edge);
+        return self;
+      },
 
-    /**
-     * 将 edge 从当前节点的边链表中删除
-     * 先找到指定的边，然后 detach 掉
-     *
-     * @param {IEdgeModelSnapshot} edge - edge 实例
-     */
-    deleteEdge(edge: IEdgeModelSnapshot) {
-      const target = self.findEdge(edge);
-      return target ? detach(target) : null;
-    }
-  }
-}).actions(self=>{
-  return {
-    /**
-     * 删除当前节点所有的边
-     *
-     */
-    deleteAllEdges() {
-      return detach(self.edges);;
-    }
-
-  }
-});
+      /**
+       * 将 edge 从当前节点的边链表中删除
+       * 先找到指定的边，然后 detach 掉
+       *
+       * @param {IEdgeModelSnapshot} edge - edge 实例
+       */
+      deleteEdge(edge: IEdgeModelSnapshot) {
+        const target = self.findEdge(edge);
+        return target ? detach(target) : null;
+      }
+    };
+  })
+  .actions(self => {
+    return {
+      /**
+       * 删除当前节点所有的边
+       *
+       */
+      deleteAllEdges() {
+        return detach(self.edges);
+      }
+    };
+  });
 
 export interface IVertexModel extends Instance<typeof VertexModel> {}
 export interface IVertexModelSnapshot
--- test/model/vertex.test.ts
+++ test/model/vertex.test.ts
@@ -1,280 +1,354 @@
-import { EdgeModel, VertexModel } from '../../src';
+import { EdgeModel, VertexModel, IVertexModel } from '../../src';
+import console = require('console');
 describe('Vertex - 节点', () => {
-    it('根据 ID 创建出节点', () => {
-        const vertex = VertexModel.create({
-            id: 'A'
-        });
-        expect(vertex).toBeDefined();
-        expect(vertex.id).toBe('A');
-        expect(vertex.key).toBe('V_A');
-        expect(vertex.edges).toEqual([]);
-        expect(vertex.meta).toEqual({});
+  it('根据 ID 创建出节点', () => {
+    const vertex = VertexModel.create({
+      id: 'A'
+    });
+    expect(vertex).toBeDefined();
+    expect(vertex.id).toBe('A');
+    expect(vertex.key).toBe('V_A');
+    expect(vertex.edges).toEqual([]);
+    expect(vertex.meta).toEqual({});
+  });
+
+  it('正常创建节点、添加边', () => {
+    const vertexA = VertexModel.create({
+      id: 'A'
+    });
+    const vertexB = VertexModel.create({
+      id: 'B'
     });
 
-    it('正常创建节点、添加边', () => {
-        const vertexA = VertexModel.create({
-            id: 'A'
-        });;
-        const vertexB = VertexModel.create({
-            id: 'B'
-        });;
-
-        const edgeAB = EdgeModel.create({
-            startVid: vertexA.id,
-            endVid: vertexB.id
-        });
-
-        vertexA.addEdge(edgeAB);
-
-        expect(vertexA.hasEdge(edgeAB)).toBe(true);
-        expect(vertexB.hasEdge(edgeAB)).toBe(false);
-        expect(vertexA.edges.length).toBe(1);
-        expect(vertexA.edges[0].id).toBe('A_B');
-        expect(vertexA.edges[0].key).toBe('E_A_B');
+    const edgeAB = EdgeModel.create({
+      startVid: vertexA.id,
+      endVid: vertexB.id
     });
 
-    it('正常可以从节点上新增、删除边', () => {
-        const vertexA = VertexModel.create({
-            id: 'A'
-        });
-        const vertexB = VertexModel.create({
-            id: 'B'
-        });
-        const vertexC = VertexModel.create({
-            id: 'C'
-        });
+    vertexA.addEdge(edgeAB);
 
-        const edgeAB = EdgeModel.create({
-            startVid: vertexA.id,
-            endVid: vertexB.id
-        });
-        const edgeAC = EdgeModel.create({
-            startVid: vertexA.id,
-            endVid: vertexC.id
-        });
+    expect(vertexA.hasEdge(edgeAB)).toBe(true);
+    expect(vertexB.hasEdge(edgeAB)).toBe(false);
+    expect(vertexA.edges.length).toBe(1);
+    expect(vertexA.edges[0].id).toBe('A_B');
+    expect(vertexA.edges[0].key).toBe('E_A_B');
+  });
 
-        vertexA.addEdge(edgeAB).addEdge(edgeAC);
-
-        // 注意，这是有向图的，存在 AB 边并不等于存在 BA 边
-        expect(vertexA.hasEdge(edgeAB)).toBe(true);
-        expect(vertexB.hasEdge(edgeAB)).toBe(false);
-
-        expect(vertexA.hasEdge(edgeAC)).toBe(true);
-        expect(vertexC.hasEdge(edgeAC)).toBe(false);
-
-        expect(vertexA.edges.length).toBe(2);
-
-        expect(vertexA.edges[0].id).toBe('A_B');
-        expect(vertexA.edges[0].key).toBe('E_A_B');
-        expect(vertexA.edges[1].id).toBe('A_C');
-        expect(vertexA.edges[1].key).toBe('E_A_C');
-
-        let removed = vertexA.deleteEdge(edgeAB);
-        expect(removed.id).toBe('A_B');
-        expect(vertexA.hasEdge(edgeAB)).toBe(false);
-        expect(vertexA.hasEdge(edgeAC)).toBe(true);
-        expect(vertexA.edges[0].id).toBe('A_C');
-
-        removed = vertexA.deleteEdge(edgeAC);
-        expect(removed.id).toBe('A_C');
-        expect(vertexA.hasEdge(edgeAB)).toBe(false);
-        expect(vertexA.hasEdge(edgeAC)).toBe(false);
-        expect(vertexA.edges.length).toBe(0);
+  it('正常可以从节点上新增、删除边', () => {
+    const vertexA = VertexModel.create({
+      id: 'A'
+    });
+    const vertexB = VertexModel.create({
+      id: 'B'
+    });
+    const vertexC = VertexModel.create({
+      id: 'C'
     });
 
-
-    it('支持从节点上删除所有的边', () => {
-        const vertexA = VertexModel.create({
-            id: 'A'
-        });
-        const vertexB = VertexModel.create({
-            id: 'B'
-        });
-        const vertexC = VertexModel.create({
-            id: 'C'
-        });
-
-        const edgeAB = EdgeModel.create({
-            startVid: vertexA.id,
-            endVid: vertexB.id
-        });
-        const edgeAC = EdgeModel.create({
-            startVid: vertexA.id,
-            endVid: vertexC.id
-        });
-
-        vertexA.addEdge(edgeAB).addEdge(edgeAC);
-
-        expect(vertexA.hasEdge(edgeAB)).toBe(true);
-        expect(vertexB.hasEdge(edgeAB)).toBe(false);
-
-        expect(vertexA.hasEdge(edgeAC)).toBe(true);
-        expect(vertexC.hasEdge(edgeAC)).toBe(false);
-
-        expect(vertexA.edges.length).toBe(2);
-
-        vertexA.deleteAllEdges();
-
-        expect(vertexA.hasEdge(edgeAB)).toBe(false);
-        expect(vertexB.hasEdge(edgeAB)).toBe(false);
-
-        expect(vertexA.hasEdge(edgeAC)).toBe(false);
-        expect(vertexC.hasEdge(edgeAC)).toBe(false);
-
-        expect(vertexA.edges.length).toBe(0);
+    const edgeAB = EdgeModel.create({
+      startVid: vertexA.id,
+      endVid: vertexB.id
+    });
+    const edgeAC = EdgeModel.create({
+      startVid: vertexA.id,
+      endVid: vertexC.id
     });
 
-    it('当节点是开始节点，支持返回当前节点的所有相邻节点', () => {
-        const vertexA = VertexModel.create({
-            id: 'A'
-        });
-        const vertexB = VertexModel.create({
-            id: 'B'
-        });
-        const vertexC = VertexModel.create({
-            id: 'C'
-        });
+    vertexA.addEdge(edgeAB).addEdge(edgeAC);
 
-        const edgeAB = EdgeModel.create({
-            startVid: vertexA.id,
-            endVid: vertexB.id
-        });
-        const edgeAC = EdgeModel.create({
-            startVid: vertexA.id,
-            endVid: vertexC.id
-        });
+    // 注意，这是有向图的，存在 AB 边并不等于存在 BA 边
+    expect(vertexA.hasEdge(edgeAB)).toBe(true);
+    expect(vertexB.hasEdge(edgeAB)).toBe(false);
 
-        vertexA.addEdge(edgeAB).addEdge(edgeAC);
+    expect(vertexA.hasEdge(edgeAC)).toBe(true);
+    expect(vertexC.hasEdge(edgeAC)).toBe(false);
 
-        // vertexB 节点对应的 edges 属性不存在，所以返回空集
-        expect(vertexB.neighborIds).toEqual([]);
+    expect(vertexA.edges.length).toBe(2);
 
-        const neighbors = vertexA.neighborIds;
+    expect(vertexA.edges[0].id).toBe('A_B');
+    expect(vertexA.edges[0].key).toBe('E_A_B');
+    expect(vertexA.edges[1].id).toBe('A_C');
+    expect(vertexA.edges[1].key).toBe('E_A_C');
 
-        expect(neighbors.length).toBe(2);
-        expect(neighbors[0]).toEqual(vertexB.id);
-        expect(neighbors[1]).toEqual(vertexC.id);
+    let removed = vertexA.deleteEdge(edgeAB);
+    expect(removed.id).toBe('A_B');
+    expect(vertexA.hasEdge(edgeAB)).toBe(false);
+    expect(vertexA.hasEdge(edgeAC)).toBe(true);
+    expect(vertexA.edges[0].id).toBe('A_C');
+
+    removed = vertexA.deleteEdge(edgeAC);
+    expect(removed.id).toBe('A_C');
+    expect(vertexA.hasEdge(edgeAB)).toBe(false);
+    expect(vertexA.hasEdge(edgeAC)).toBe(false);
+    expect(vertexA.edges.length).toBe(0);
+  });
+
+  it('支持从节点上删除所有的边', () => {
+    const vertexA = VertexModel.create({
+      id: 'A'
+    });
+    const vertexB = VertexModel.create({
+      id: 'B'
+    });
+    const vertexC = VertexModel.create({
+      id: 'C'
     });
 
-    it('当节点是结束节点，也能获取相邻节点属性', () => {
-        const vertexA = VertexModel.create({
-            id: 'A'
-        });
-        const vertexB = VertexModel.create({
-            id: 'B'
-        });
-        const vertexC = VertexModel.create({
-            id: 'C'
-        });
-
-        const edgeAB = EdgeModel.create({
-            startVid: vertexA.id,
-            endVid: vertexB.id
-        });
-        const edgeAC = EdgeModel.create({
-            startVid: vertexA.id,
-            endVid: vertexC.id
-        });
-
-        vertexA.addEdge(edgeAB).addEdge(edgeAC);
-
-
-        expect(vertexB.neighborIds).toEqual([]);
-
-        const neighbors = vertexA.neighborIds;
-
-        expect(neighbors.length).toBe(2);
-        expect(neighbors[0]).toEqual(vertexB.id);
-        expect(neighbors[1]).toEqual(vertexC.id);
+    const edgeAB = EdgeModel.create({
+      startVid: vertexA.id,
+      endVid: vertexB.id
+    });
+    const edgeAC = EdgeModel.create({
+      startVid: vertexA.id,
+      endVid: vertexC.id
     });
 
-    it('检查当前节点是否存在指定的相邻节点', () => {
-        const vertexA = VertexModel.create({
-            id: 'A'
-        });
-        const vertexB = VertexModel.create({
-            id: 'B'
-        });
-        const vertexC = VertexModel.create({
-            id: 'C'
-        });
+    vertexA.addEdge(edgeAB).addEdge(edgeAC);
 
-        const edgeAB = EdgeModel.create({
-            startVid: vertexA.id,
-            endVid: vertexB.id
-        });
-        vertexA.addEdge(edgeAB)
+    expect(vertexA.hasEdge(edgeAB)).toBe(true);
+    expect(vertexB.hasEdge(edgeAB)).toBe(false);
 
-        expect(vertexA.hasNeighbor(vertexB)).toBe(true);
-        expect(vertexA.hasNeighbor(vertexC)).toBe(false);
+    expect(vertexA.hasEdge(edgeAC)).toBe(true);
+    expect(vertexC.hasEdge(edgeAC)).toBe(false);
+
+    expect(vertexA.edges.length).toBe(2);
+
+    vertexA.deleteAllEdges();
+
+    expect(vertexA.hasEdge(edgeAB)).toBe(false);
+    expect(vertexB.hasEdge(edgeAB)).toBe(false);
+
+    expect(vertexA.hasEdge(edgeAC)).toBe(false);
+    expect(vertexC.hasEdge(edgeAC)).toBe(false);
+
+    expect(vertexA.edges.length).toBe(0);
+  });
+
+  it('当节点是开始节点，支持返回当前节点的所有相邻节点', () => {
+    const vertexA = VertexModel.create({
+      id: 'A'
+    });
+    const vertexB = VertexModel.create({
+      id: 'B'
+    });
+    const vertexC = VertexModel.create({
+      id: 'C'
     });
 
-
-    it('能通过当前节点找到指定边', () => {
-        const vertexA = VertexModel.create({
-            id: 'A'
-        });
-        const vertexB = VertexModel.create({
-            id: 'B'
-        });
-        const vertexC = VertexModel.create({
-            id: 'C'
-        });
-
-        const edgeAB = EdgeModel.create({
-            startVid: vertexA.id,
-            endVid: vertexB.id
-        });
-        vertexA.addEdge(edgeAB)
-
-        expect(vertexA.findEdgeByVertex(vertexB)).toEqual(edgeAB);
-        expect(vertexA.findEdgeByVertex(vertexC)).toBeNull();
+    const edgeAB = EdgeModel.create({
+      startVid: vertexA.id,
+      endVid: vertexB.id
+    });
+    const edgeAC = EdgeModel.create({
+      startVid: vertexA.id,
+      endVid: vertexC.id
     });
 
-    it('获取节点的 degree 数值', () => {
-        const vertexA = VertexModel.create({
-            id: 'A'
-        });
-        const vertexB = VertexModel.create({
-            id: 'B'
-        });
-        expect(vertexA.degree).toBe(0);
+    vertexA.addEdge(edgeAB).addEdge(edgeAC);
 
-        const edgeAB = EdgeModel.create({
-            startVid: vertexA.id,
-            endVid: vertexB.id
-        });
-        vertexA.addEdge(edgeAB);
+    // vertexB 节点对应的 edges 属性不存在，所以返回空集
+    expect(vertexB.neighborIds).toEqual([]);
 
-        expect(vertexA.degree).toBe(1);
+    const neighbors = vertexA.neighborIds;
 
-        const edgeBA = EdgeModel.create({
-            startVid: vertexB.id,
-            endVid: vertexA.id
-        });;
-        vertexA.addEdge(edgeBA);
+    expect(neighbors.length).toBe(2);
+    expect(neighbors[0]).toEqual(vertexB.id);
+    expect(neighbors[1]).toEqual(vertexC.id);
+  });
 
-        expect(vertexA.degree).toBe(2);
+  it('当节点是结束节点，也能获取相邻节点属性', () => {
+    const vertexA = VertexModel.create({
+      id: 'A'
+    });
+    const vertexB = VertexModel.create({
+      id: 'B'
+    });
+    const vertexC = VertexModel.create({
+      id: 'C'
     });
 
-    it('重复添加同一条边将报错', ()=>{
-          const vertexA = VertexModel.create({
-            id: 'A'
-        });
-        const vertexB = VertexModel.create({
-            id: 'B'
-        });
+    const edgeAB = EdgeModel.create({
+      startVid: vertexA.id,
+      endVid: vertexB.id
+    });
+    const edgeAC = EdgeModel.create({
+      startVid: vertexA.id,
+      endVid: vertexC.id
+    });
 
-        const edgeAB = EdgeModel.create({
-            startVid: vertexA.id,
-            endVid: vertexB.id
-        });
-        vertexA.addEdge(edgeAB);
+    vertexA.addEdge(edgeAB).addEdge(edgeAC);
 
-        expect(()=>{
-            vertexA.addEdge(edgeAB);
-        }).toThrow();
+    expect(vertexB.neighborIds).toEqual([]);
 
-    })
+    const neighbors = vertexA.neighborIds;
 
+    expect(neighbors.length).toBe(2);
+    expect(neighbors[0]).toEqual(vertexB.id);
+    expect(neighbors[1]).toEqual(vertexC.id);
+  });
+
+  it('检查当前节点是否存在指定的相邻节点', () => {
+    const vertexA = VertexModel.create({
+      id: 'A'
+    });
+    const vertexB = VertexModel.create({
+      id: 'B'
+    });
+    const vertexC = VertexModel.create({
+      id: 'C'
+    });
+
+    const edgeAB = EdgeModel.create({
+      startVid: vertexA.id,
+      endVid: vertexB.id
+    });
+    vertexA.addEdge(edgeAB);
+
+    expect(vertexA.hasNeighbor(vertexB)).toBe(true);
+    expect(vertexA.hasNeighbor(vertexC)).toBe(false);
+  });
+
+  it('能通过当前节点找到指定边', () => {
+    const vertexA = VertexModel.create({
+      id: 'A'
+    });
+    const vertexB = VertexModel.create({
+      id: 'B'
+    });
+    const vertexC = VertexModel.create({
+      id: 'C'
+    });
+
+    const edgeAB = EdgeModel.create({
+      startVid: vertexA.id,
+      endVid: vertexB.id
+    });
+    vertexA.addEdge(edgeAB);
+
+    expect(vertexA.findEdgeByVertex(vertexB)).toEqual(edgeAB);
+    expect(vertexA.findEdgeByVertex(vertexC)).toBeNull();
+  });
+
+  it('获取节点的 degree 数值', () => {
+    const vertexA = VertexModel.create({
+      id: 'A'
+    });
+    const vertexB = VertexModel.create({
+      id: 'B'
+    });
+    expect(vertexA.degree).toBe(0);
+
+    const edgeAB = EdgeModel.create({
+      startVid: vertexA.id,
+      endVid: vertexB.id
+    });
+    vertexA.addEdge(edgeAB);
+
+    expect(vertexA.degree).toBe(1);
+
+    const edgeBA = EdgeModel.create({
+      startVid: vertexB.id,
+      endVid: vertexA.id
+    });
+    vertexA.addEdge(edgeBA);
+
+    expect(vertexA.degree).toBe(2);
+  });
+
+  it('重复添加同一条边将报错', () => {
+    const vertexA = VertexModel.create({
+      id: 'A'
+    });
+    const vertexB = VertexModel.create({
+      id: 'B'
+    });
+
+    const edgeAB = EdgeModel.create({
+      startVid: vertexA.id,
+      endVid: vertexB.id
+    });
+    vertexA.addEdge(edgeAB);
+
+    expect(() => {
+      vertexA.addEdge(edgeAB);
+    }).toThrow();
+  });
+
+  describe.only('clone 节点', () => {
+    let vertexA: IVertexModel;
+    beforeEach(() => {
+      vertexA = VertexModel.create({
+        id: 'A'
+      });
+      vertexA.setMeta({
+        hello: 'world'
+      });
+
+      const vertexB = VertexModel.create({
+        id: 'B'
+      });
+      const edgeAB = EdgeModel.create({
+        startVid: vertexA.id,
+        endVid: vertexB.id
+      });
+      vertexA.addEdge(edgeAB);
+    });
+
+    it('默认拷贝的节点数据和原来保持一致', () => {
+      const cloned = vertexA.clone();
+
+      expect(vertexA.id).toBe('A');
+      expect(vertexA.edges.length).toBe(1);
+      expect(vertexA.edges[0].id).toBe('A_B');
+      expect(vertexA.meta).toEqual({
+        hello: 'world'
+      });
+      expect(cloned.toJSON()).toEqual(vertexA.toJSON());
+    });
+
+    it('拷贝时可指定 clone 函数', () => {
+      let count1 = 1;
+      let count2 = 1;
+      const cloneId = (id: string) => {
+        return `${id}${count1++}`;
+      };
+      const cloneMeta = (meta: any) => {
+        return { foo: `bar${count2++}`, ...meta };
+      };
+
+      const cloned1 = vertexA.clone({
+        cloneId,
+        cloneMeta
+      });
+
+      const cloned2 = vertexA.clone({
+        cloneId,
+        cloneMeta
+      });
+
+      //   原来节点不受影响
+      expect(vertexA.id).toBe('A');
+      expect(vertexA.edges.length).toBe(1);
+      expect(vertexA.edges[0].id).toBe('A_B');
+      expect(vertexA.meta).toEqual({
+        hello: 'world'
+      });
+
+      expect(cloned1.id).toBe('A1');
+      expect(cloned1.edges.length).toBe(1);
+      expect(cloned1.edges[0].id).toBe('A1_B');
+      expect(cloned1.meta).toEqual({
+          hello: 'world',
+        foo: `bar1`
+      });
+
+      expect(cloned2.id).toBe('A2');
+      expect(cloned2.edges.length).toBe(1);
+      expect(cloned2.edges[0].id).toBe('A2_B');
+      expect(cloned2.meta).toEqual({
+          hello: 'world',
+        foo: `bar2`
+      });
+    });
+  });
 });
